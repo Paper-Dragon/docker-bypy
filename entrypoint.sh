976583e493a2df4504bebda26a0ce3d37fb1d50e
PaperDragon <2678885646@qq.com>
2024-08-21 21:41:15 +0800

    bug fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ CRON_SCHEDULE=${CRON_SCHEDULE:-"0 0 * * *"} # Default schedule: every day at mid
 # Function to check if the cron job already exists
 check_cron_job_exists() {
   CRON_LIST=$(crontab -l)
-  if [[ $CRON_LIST =~ $1 ]]; then
+  if echo "$CRON_LIST" | grep -qF "$1"; then
     return 0
   else
     return 1
@@ -27,7 +27,7 @@ check_cron_job_exists() {
 }
 
 # Add Cron job
-if ! check_cron_job_exists "${CRON_SCHEDULE} .*"; then
+if ! check_cron_job_exists "bypy"; then
     (
     crontab -l
     echo "${CRON_SCHEDULE} /bin/sh -c 'DIR=/apps TAR_FILE=/apps-$(date +%Y%m%d%H%M).tar.gz tar -czf \${TAR_FILE}  \${DIR} . && bypy -v upload \${TAR_FILE} backup/ && rm \${TAR_FILE}' >> /proc/1/fd/1 2>&1"
